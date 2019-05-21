219c849cdce759f7d3504c7c6a7a07060c395e66
Danny Simantov <danny@elminda.com>
2019-05-21 14:24:32 +0300

    Added auto-management of local chromedriver.

--- README.md
+++ README.md
@@ -2,17 +2,7 @@
 
 This example currently supports Linux and Windows, both with Java 8 and above, Maven and Chrome browser of version 69-71 installed.
 
-To run on Windows, simply clone the repository and from root project diectory run:
-```
-mvn test
-```
-
-To run on Linux, make the chromedriver file executable by running the following command from root project dir:
-```
-chmod +x src/main/java/com/github/automatedowl/examples/drivers/executable/chromedriver
-```
-
-Then in order to run the test:
+To run on Windows or Linux, simply clone the repository and from root project diectory run:
 ```
 mvn test
 ```
--- src/main/java/com/github/automatedowl/examples/drivers/DriverModule.java
+++ src/main/java/com/github/automatedowl/examples/drivers/DriverModule.java
@@ -1,6 +1,7 @@
 package com.github.automatedowl.examples.drivers;
 
 import com.google.inject.AbstractModule;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.tika.utils.SystemUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,10 +10,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
  * Guice configuration module for WebDriver object.
  */
 public class DriverModule extends AbstractModule {
-
-    // Define String of chromedriver system property.
-    private static final String CHROMEDRIVER_SYSTEM_PROPERTY =
-            "webdriver.chrome.driver";
 
     /** Configure method. It binds a selected type of BrowserDriver. */
     protected synchronized void configure() {
@@ -25,17 +22,6 @@ public class DriverModule extends AbstractModule {
     }
 
     private void setChromedriverPath() {
-        System.setProperty(CHROMEDRIVER_SYSTEM_PROPERTY, getChromedriverPath());
-    }
-
-    public static String getChromedriverPath() {
-        if (SystemUtils.IS_OS_WINDOWS) {
-          return System.getProperty("user.dir")
-                  + "/src/main/java/com/github/automatedowl" +
-                  "/examples/drivers/executable/chromedriver.exe";
-        }
-        return System.getProperty("user.dir")
-                + "/src/main/java/com/github/automatedowl" +
-                "/examples/drivers/executable/chromedriver";
+        WebDriverManager.chromedriver().setup();
     }
 }
