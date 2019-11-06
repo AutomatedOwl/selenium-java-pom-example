923e772a578e7d276cf3a33ce13fad91c8b08f0e
Hadar Greenberg <hadar@il.elminda.com>
2019-11-06 15:34:02 +0200

    changing to remote driver

--- src/main/java/com/github/automatedowl/examples/drivers/DriverModule.java
+++ src/main/java/com/github/automatedowl/examples/drivers/DriverModule.java
@@ -4,6 +4,11 @@ import com.google.inject.AbstractModule;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Guice configuration module for WebDriver object.
@@ -18,10 +23,15 @@ public class DriverModule extends AbstractModule {
 
 
         // Bind WebDriver object to ChromeDriver.
-        bind(WebDriver.class).toInstance(new ChromeDriver());
+        try {
+            bind(WebDriver.class).toInstance(new RemoteWebDriver(
+                    new URL("http://zalenium.elminda.com/wd/hub"), new ChromeOptions()));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
     private void setChromedriverPath() {
-        WebDriverManager.chromedriver().setup();
+        //WebDriverManager.chromedriver().setup();
     }
 }
