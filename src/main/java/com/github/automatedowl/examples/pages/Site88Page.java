6b37a4c3cbcae650541ccdcc5d9106ddb03370b7
AutomatedOwl <automatedowl@yandex.com>
2020-07-02 09:11:07 +0300

    Update Site88Page.java

--- src/main/java/com/github/automatedowl/examples/pages/Site88Page.java
+++ src/main/java/com/github/automatedowl/examples/pages/Site88Page.java
@@ -13,7 +13,7 @@ public class Site88Page {
 	/** Default timeout of waiting for web elements in the page. */
 	private final int DEFAULT_ELEMENT_TIMEOUT = 30;
 	private final String XPATH_CHILD_ELEMENTS_LOCATOR = ".//*";
-	private final String URL = "https://testjs2.000webhostapp.com/";
+	private final String URL = "http://testjs.epizy.com/";
 
 	/////////////////////////////////////////////////////////////////////////////////
 	/////////////////////////////// Constructor /////////////////////////////////////
